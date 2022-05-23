2d3497b60204648c7f20340fda603adf1a248e01
Ricardo Miralles <ricmiber@gmail.com>
2022-05-23 13:04:59 +0200

    Add Heroku config

--- server/src/loggerMiddleware.js
+++ server/src/loggerMiddleware.js
@@ -1,8 +1,8 @@
 const logger = (req, res, next) => {
-    console.log(req.method)
-    console.log(req.path)
-    console.log(req.body)
-    next()
-  }
+  console.log(req.method)
+  console.log(req.path)
+  console.log(req.body)
+  next()
+}
 
 module.exports = logger
