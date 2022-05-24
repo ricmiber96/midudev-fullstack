dd3efc054f94c3ceb6cf2b699c81b0ad83778002
Ricardo Miralles <ricmiber@gmail.com>
2022-05-24 20:46:27 +0200

    Update fourth lesson

---
+++ server/src/__tests__/average.test.js
@@ -0,0 +1,20 @@
+const { average } = require('../utils/for_testing')
+
+describe('Average', () => {
+  test('of one value is the value of itself', () => {
+    const result = average([2])
+    expect(result).toBe(2)
+  })
+  test('of many is calculated correctly', () => {
+    const result = average([1, 2, 3, 4, 5, 6])
+    expect(result).toBe(3.5)
+  })
+  test('of empty array is calculated correctly', () => {
+    const result = average([])
+    expect(result).toBe(0)
+  })
+  test('of empty undefined is calculated correctly', () => {
+    const result = average()
+    expect(result).toBe(0)
+  })
+})
